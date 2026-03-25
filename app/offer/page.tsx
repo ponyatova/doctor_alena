ffb5431721555834de56815ab6a25fbb11cbe519
ponyatova <ponyatova.95@mail.ru>
2026-03-25 21:20:17 +0400

    Update page.tsx

--- app/offer/page.tsx
+++ app/offer/page.tsx
@@ -1,7 +1,7 @@
 import ClientPageOffer from "./ClientPageOffer";
 
 export default async function OfferPage() {
-  const url = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const url = await process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
   const email = process.env.NEXT_PUBLIC_CONTACT_EMAIL || "";
   return <ClientPageOffer url={url} email={email} />;
 }
